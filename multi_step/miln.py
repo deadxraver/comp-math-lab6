0c09885dcae800bc9f6c070fff97247e3f67751e
Даниил <deadxraver@yandex.ru>
2025-06-10 01:19:39 +0300

    miln logging done, todo: user input

--- multi_step/miln.py
+++ multi_step/miln.py
@@ -7,21 +7,27 @@ def solution(x0, y0, h, xn, f_ind, eps, c):
 	y_arr = [y0]
 	f = equations.derivatives[f_ind]
 	f_arr = [f(x0, y0)]
+	precise_f = equations.precise[f_ind]
+	print(f'Первые 3 точки с помощью Рунге-Кутта 4 порядка с шагом {h}:\n|\tx\t|\ty\t|\ty_p\t|\tdiff\t|')
 	for i in range(3):
 		x, y = one_step.rk4.step(equations.derivatives[f_ind], h, x_arr[-1], y_arr[-1])
+		print(f'|\t{x:.1f}\t|\t{y:.1f}\t|\t{precise_f(x, y, c):.1f}\t|\t{abs(precise_f(x, y, c) - y):.3f}\t|')
 		x_arr.append(x)
 		y_arr.append(y)
 		f_arr.append(f(x, y))
 	if x_arr[-1] > xn:
 		print("Слишком короткий промежуток или слишком большой шаг, вышли за предел при вычислении первых трех точек!")
 		return None, None
+	print(f'Милн с шагом {h}:\n|\tx\t|\ty\t|\ty_p\t|\tdiff\t|')
 	while x_arr[-1] <= xn:
 		y_pred = y_arr[-4] + 4 * h / 3 * (2 * f_arr[-3] - f_arr[-2] + 2 * f_arr[-1])
 		x_arr.append(x_arr[-1] + h)
 		f_pred = f(x_arr[-1], y_pred)
 		f_arr.append(f_pred)
 		y_arr.append(y_arr[-2] + h / 3 * (f_arr[-2] + 4 * f_arr[-1] + f_pred))
-		diff = abs(y_arr[-1] - equations.precise[f_ind](x_arr[-1], y_arr[-1], c))
+		diff = abs(y_arr[-1] - precise_f(x_arr[-1], y_arr[-1], c))
+		print(
+			f'|\t{x_arr[-1]:.1f}\t|\t{y_arr[-1]:.1f}\t|\t{precise_f(x_arr[-1], y_arr[-1], c):.1f}\t|\t{abs(precise_f(x_arr[-1], y_arr[-1], c) - y_arr[-1]):.3f}\t|')
 		if diff > eps:
 			print(f'Вышли за допустимую точность ({diff} > {eps}), пересчитаем с меньшим шагом')
 			return solution(x0, y0, h / 2, xn, f_ind, eps, c)
