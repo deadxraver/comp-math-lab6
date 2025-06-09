4ea65d9a97889a886320278e935053e268329b13
Даниил <deadxraver@yandex.ru>
2025-06-10 00:58:27 +0300

    miln logic done, todo: diff + console logging

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import equations
 import graph
 import one_step.euler
 import one_step.rk4
+import multi_step.miln
 
 # Это будет вводиться с клавиатуры
 f_ind = 1
 x0 = 0
-xn = 0.2
+xn = 2
 y0 = 1
 h = 0.1
 eps = 0.01
@@ -20,8 +21,9 @@ graph.add_function(lambda x: precise_f(x, 0, c), [x0, xn], label=f'Точное,
 
 euler_x_arr, euler_y_arr = one_step.euler.solution(x0, y0, h, xn, f_ind, eps, c)
 rk4_x_arr, rk4_y_arr = one_step.rk4.solution(x0, y0, h, xn, f_ind, eps, c)
-
+miln_x_arr, miln_y_arr = multi_step.miln.solution(x0, y0, h, xn, f_ind, eps, c)
 
 graph.add_points(euler_x_arr, euler_y_arr, color='red', label='Эйлер')
 graph.add_points(rk4_x_arr, rk4_y_arr, color='green', label='Рунге-Кутта 4 порядка')
+if miln_x_arr is not None: graph.add_points(miln_x_arr, miln_y_arr, color='blue', label='Милн')
 graph.show()
---
+++ multi_step/miln.py
@@ -0,0 +1,24 @@
+import equations
+import one_step.rk4
+
+
+def solution(x0, y0, h, xn, f_ind, eps, c):
+	x_arr = [x0]
+	y_arr = [y0]
+	f = equations.derivatives[f_ind]
+	f_arr = [f(x0, y0)]
+	for i in range(3):
+		x, y = one_step.rk4.step(equations.derivatives[f_ind], h, x_arr[-1], y_arr[-1])
+		x_arr.append(x)
+		y_arr.append(y)
+		f_arr.append(f(x, y))
+	if x_arr[-1] > xn:
+		print("Слишком короткий промежуток или слишком большой шаг, вышли за предел при вычислении первых трех точек!")
+		return None, None
+	while x_arr[-1] < xn:
+		y_pred = y_arr[-4] + 4 * h / 3 * (2 * f_arr[-3] - f_arr[-2] + 2 * f_arr[-1])
+		x_arr.append(x_arr[-1] + h)
+		f_pred = f(x_arr[-1], y_pred)
+		f_arr.append(f_pred)
+		y_arr.append(y_arr[-2] + h / 3 * (f_arr[-2] + 4 * f_arr[-1] + f_pred))
+	return x_arr, y_arr
