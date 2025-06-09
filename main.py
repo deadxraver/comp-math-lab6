c8cd2ac7b28588ba1c1fdbbce13487a6e544e1e5
Даниил <deadxraver@yandex.ru>
2025-06-10 00:02:16 +0300

    euler method done + runge

--- main.py
+++ main.py
@@ -2,21 +2,23 @@ import equations
 import graph
 import one_step.euler
 
-euler_x_arr = []
-euler_y_arr = []
+# Это будет вводиться с клавиатуры
 f_ind = 1
-x = 0
-xn = 6
-y = 1
+x0 = 0
+xn = 0.2
+y0 = 1
 h = 0.1
-eps = 0.001
-c = y - equations.precise[f_ind](x, 0)
+eps = 0.01
+
+# Это будем использовать
+precise_f = equations.precise[f_ind]
+c = y0 - precise_f(x0, 0)
+
 graph.create_plot()
-graph.add_function(lambda x: equations.precise[f_ind](x, c), [x, xn], label='Точное', color='red')
-while x < xn:
-	euler_x_arr.append(x)
-	euler_y_arr.append(y)
-	x, y = one_step.euler.step(equations.derivatives[f_ind], h, x, y)
+graph.add_function(lambda x: precise_f(x, c), [x0, xn], label='Точное', color='red')
+euler_x_arr, euler_y_arr = one_step.euler.solution(x0, y0, h, xn, f_ind, eps, c)
+
+
 
 graph.add_points(euler_x_arr, euler_y_arr, color='black', label='Эйлер')
 graph.show()
