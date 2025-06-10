5bf9ea99f781aca44c81359f1118c330d3ff2619
Даниил <deadxraver@yandex.ru>
2025-06-11 00:16:27 +0300

    fix miln + more verbose output

--- one_step/solver.py
+++ one_step/solver.py
@@ -11,16 +11,17 @@ def solution(x0, y0, h, xn, f_ind, eps, c, step, name, p):
 	while True:
 		x = x0
 		y = y0
-		print(f'{name} с шагом {h1}:\n|\tx\t|\ty\t|\ty_p\t|\tdiff\t|')
+		print(f'{name} с шагом {h1}:\n|\tx\t\t|\ty\t\t|\ty_p\t\t|\tdiff\t|')
 		if len(y_arr):
 			yn = y_arr[-1]
 		y_arr = []
 		x_arr = []
+
 		while x <= xn:
-			print(f'|\t{x:.1f}\t|\t{y:.1f}\t|\t{precise_f(x, y, c):.1f}\t|\t{abs(precise_f(x, y, c) - y):.3f}\t|')
+			x, y = step(equations.derivatives[f_ind], h1, x, y)
+			print(f'|\t{x:.3f}\t|\t{y:.3f}\t|\t{precise_f(x, y, c):.3f}\t|\t{abs(precise_f(x, y, c) - y):.3f}\t|')
 			x_arr.append(x)
 			y_arr.append(y)
-			x, y = step(equations.derivatives[f_ind], h1, x, y)
 		if yn is not None:
 			runge = abs(yn - y_arr[-1]) / (2 ** p - 1)
 		print(f'y^h={yn}, y^h/2={y_arr[-1]}')
